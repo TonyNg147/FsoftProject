f06fb5f35032c40a0c940ee67c8ca92ab1a4e19e
lhp1507 <lehuuphu15dt3@gmail.com>
2021-06-10 23:01:22 +0700

    PhuLH1 - Waiting msg dialog

--- Source/BankingProject/BankingProject/BankingDefine.h
+++ Source/BankingProject/BankingProject/BankingDefine.h
@@ -1,10 +1,10 @@
 #pragma once
 
-//Define Infomation
+//Define Infomation - do not remove
 #define WIDTH_APP 1024
 #define HEIGHT_APP 768
 
-// Define Infomation for Button Test
+// Define Infomation for Button Test - for test
 #define WIDTH_BUT_TEST 200
 #define HEIGHT_BUT_TEST 100
 #define X_BUT_TEST 300
--- Source/BankingProject/BankingProject/BankingProjectDlg.cpp
+++ Source/BankingProject/BankingProject/BankingProjectDlg.cpp
@@ -1,4 +1,4 @@
-
+﻿
 // BankingProjectDlg.cpp : implementation file
 //
 
@@ -20,6 +20,7 @@
 class CAboutDlg : public CDialogEx
 {
 public:
+	BOOL OnInitDialog();
 	CAboutDlg();
 
 // Dialog Data
@@ -34,6 +35,13 @@ public:
 protected:
 	DECLARE_MESSAGE_MAP()
 };
+
+BOOL CAboutDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+	CenterWindow();
+	return TRUE;
+}
 
 CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
 {
@@ -66,8 +74,8 @@ void CBankingProjectDlg::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CBankingProjectDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_COMMAND(ID_PHULH1_WAITINGSCR, &CBankingProjectDlg::OnPhulh1Waitingscr)
 END_MESSAGE_MAP()
 
 
@@ -76,6 +84,8 @@ END_MESSAGE_MAP()
 BOOL CBankingProjectDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
+
+	
 
 	// Add "About..." menu item to system menu.
 
@@ -106,6 +116,18 @@ BOOL CBankingProjectDlg::OnInitDialog()
 	
 	//Set Layout when start App
 	LayoutControl();
+
+	/*
+		Begin PhuLH1: 10.06
+	*/
+	m_waitingMsgDlg.Create(IDD_DIALOG_MSG_WAITING, this);
+
+	m_menu.LoadMenuW(IDR_MENU_MAIN);
+
+	SetMenu(&m_menu);
+	/*
+		End PhuLH1: 10.06
+	*/
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -161,9 +183,41 @@ HCURSOR CBankingProjectDlg::OnQueryDragIcon()
 
 void CBankingProjectDlg::LayoutControl()
 {
-	CRect rect;
-	GetClientRect(&rect);
-	MoveWindow(rect.left, rect.top, WIDTH_APP, HEIGHT_APP);
+	/*
+		PhuLH1: 
+		http://www.equestionanswers.com/vcpp/dialog-middle-of-desktop.php
+		https://stackoverflow.com/questions/771109/how-to-move-controls-to-the-middle-of-an-mfc-form
+	*/
+
+	
+	CRect rectControl;
+	GetClientRect(&rectControl);
+	MoveWindow(rectControl.left, rectControl.top, WIDTH_APP, HEIGHT_APP);
 	m_btTest.MoveWindow(X_BUT_TEST, Y_BUT_TEST, WIDTH_BUT_TEST, HEIGHT_BUT_TEST);
+	
+	
+	/* PhuLH1: test MoveWindow by width/height (not work)
+	CRect rectControl;
+	this->GetClientRect(&rectControl);
+
+	int x = ((rectControl.right - rectControl.left) - WIDTH_APP) / 2;
+	int y = ((rectControl.bottom - rectControl.top) - HEIGHT_APP) / 2;
+
+	MoveWindow(x, y, WIDTH_APP, HEIGHT_APP);
+	m_btTest.MoveWindow(X_BUT_TEST, Y_BUT_TEST, WIDTH_BUT_TEST, HEIGHT_BUT_TEST);
+	*/
 }
 
+
+/*
+	Begin PhuLH1: 10.06
+*/
+void CBankingProjectDlg::OnPhulh1Waitingscr()
+{
+	// TODO: Add your command handler code here
+	CWaitingMsgDialog waitingDlg;
+	waitingDlg.DoModal();
+}
+/*
+	End PhuLH1: 10.06
+*/
